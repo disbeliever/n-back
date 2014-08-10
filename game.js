7b25f47158b91aacbae980ebb926dbdf4fcbcef3
disbeliever <nerevar101@mail.ru>
2014-08-10 17:23:35 +0400

    basic drawing

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
+const showTime = 500;
+
 function init() {
   var ctx = document.getElementById("nback").getContext("2d");
 
   var bgReady = false;
   var bgImage = new Image();
+  bgImage.src = "res/square.png";
+
   bgImage.onload = function () {
     bgReady = true;
 
@@ -12,14 +16,20 @@ function init() {
       }
     }
 
+    function drawAndClear()
+    {
+      rand_i = Math.floor((Math.random() * 3));
+      rand_j = Math.floor((Math.random() * 3));
+      ctx.drawImage(bgImage, 64, 0, 64, 64, rand_i*64, rand_j*64, 64, 64);
+      setTimeout(function() { ctx.drawImage(bgImage, 0, 0, 64, 64, rand_i*64, rand_j*64, 64, 64) }, showTime);
+    }
+
+    setInterval(drawAndClear, showTime * 2);
   };
-  bgImage.src = "res/square.png";
 
   //ctx.fillStyle = "rgb(0, 0, 0)";
   //ctx.font = "24px Helvetica";
   //ctx.textAlign = "left";
   //ctx.textBaseline = "top";
-  //ctx.fillText("test", 32, 32);
-
+  //ctx.fillText(showTime, 32, 32);
 };
-
