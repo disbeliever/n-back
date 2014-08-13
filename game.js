494eecbfdb1b2bc55fcc0c758833f1240cb72ada
disbeliever <nerevar101@mail.ru>
2014-08-13 21:20:42 +0400

    don't allow to count as correct and wrong at the same time. Started implementing the 'stop' button functionality. Some reindents

--- game.js
+++ game.js
@@ -2,66 +2,84 @@ const showTime = 500;
 const N = 2;
 var ctx;
 var bgImage;
+var started = false;
 
 function startGame()
 {
-  i_vals = Array();
-    j_vals = Array();
-    key_pressed = false;
-    function drawAndClear()
-    {
-      key_pressed = false;
-      rand_i = Math.floor((Math.random() * 3));
-      rand_j = Math.floor((Math.random() * 3));
-      i_vals.push(rand_i);
-      j_vals.push(rand_j);
-      if (i_vals.length > N + 1)
-      {
-        i_vals.shift();
-        j_vals.shift();
-      }
-      ctx.drawImage(bgImage, 64, 0, 64, 64, rand_i*64, rand_j*64, 64, 64);
-      setTimeout(function() {
-        if (!key_pressed && i_vals.length > N && i_vals[0] == rand_i && j_vals[0] == rand_j)
-        {
-          wrong++;
-          document.getElementById("wrong").innerHTML = wrong;
-        }
-        ctx.drawImage(bgImage, 0, 0, 64, 64, rand_i*64, rand_j*64, 64, 64) }, showTime);
-      //console.log(i_vals);
-    }
+  var correct = 0;
+  var wrong = 0;
+  var set_to;
+  var set_int;
+  if (started)
+  {
+    started = false;
+    document.getElementById("startButton").innerHTML = "Start";
+    removeEventListener("keydown");
+    clearInterval(set_int);
+    clearTimeout(set_to);
+    return;
+  }
+  else 
+  {
+    started = true;
+    document.getElementById("startButton").innerHTML = "Stop";
+  }
 
-    addEventListener("keydown", function (e) {
-      //keysDown[e.keyCode] = true;
-      if (e.keyCode == 32 && i_vals.length > N)
+
+  i_vals = Array();
+  j_vals = Array();
+  key_pressed = false;
+  function drawAndClear()
+  {
+    key_pressed = false;
+    rand_i = Math.floor((Math.random() * 3));
+    rand_j = Math.floor((Math.random() * 3));
+    i_vals.push(rand_i);
+    j_vals.push(rand_j);
+    if (i_vals.length > N + 1)
+    {
+      i_vals.shift();
+      j_vals.shift();
+    }
+    ctx.drawImage(bgImage, 64, 0, 64, 64, rand_i*64, rand_j*64, 64, 64);
+    set_to = setTimeout(function() {
+      if (!key_pressed && i_vals.length > N && i_vals[0] == rand_i && j_vals[0] == rand_j)
       {
         key_pressed = true;
-        //console.log(i_vals);
-        //console.log(j_vals);
-        if (i_vals[0] == rand_i && j_vals[0] == rand_j)
-        {
-          correct++;
-          document.getElementById("correct").innerHTML = correct;
-          //console.log("correct");
-        }
-        else
-        {
-          wrong++;
-          document.getElementById("wrong").innerHTML = wrong;
-          //console.log("wrong");
-        }
+        wrong++;
+        document.getElementById("wrong").innerHTML = wrong;
       }
-    }, false);
+      ctx.drawImage(bgImage, 0, 0, 64, 64, rand_i*64, rand_j*64, 64, 64) }, showTime);
+    //console.log(i_vals);
+  }
 
-    setInterval(drawAndClear, showTime * 3);
+  addEventListener("keydown", function (e) {
+    //keysDown[e.keyCode] = true;
+    if (e.keyCode == 32 && i_vals.length > N && !key_pressed)
+    {
+      key_pressed = true;
+      if (i_vals[0] == rand_i && j_vals[0] == rand_j)
+      {
+        correct++;
+        document.getElementById("correct").innerHTML = correct;
+        //console.log("correct");
+      }
+      else
+      {
+        wrong++;
+        document.getElementById("wrong").innerHTML = wrong;
+        //console.log("wrong");
+      }
+    }
+  }, false);
+
+  set_int = setInterval(drawAndClear, showTime * 3);
 }
 
 function init()
 {
   ctx = document.getElementById("nback").getContext("2d");
 
-  var correct = 0;
-  var wrong = 0;
   var bgReady = false;
   bgImage = new Image();
   bgImage.src = "res/square.png";
